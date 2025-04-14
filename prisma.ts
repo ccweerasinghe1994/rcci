06a51e3a0cb302c1189fbe62a820d2a6e521be40
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-04-14 16:53:55 +0530

    Remove experimental nodeMiddleware option from Next.js configuration and update dependencies for PostgreSQL integration, including adding @prisma/adapter-pg and pg packages. Adjust Prisma schema to enable driver adapters and ensure proper connection string formatting.

--- prisma.ts
+++ prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@/lib/generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = `${process.env.DATABASE_URL}`;
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
