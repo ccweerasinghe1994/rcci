c672f235e397506c78bfa5343b43fbe8332d60af
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-04-14 15:56:52 +0530

    Add @types/pg and export prisma client

--- prisma.ts
+++ prisma.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const connectionString = process.env.DATABASE_URL;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter });
