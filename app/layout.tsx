c3b7cf06e346cd585ae4830b6041e548518dc7d1
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-04-15 23:15:46 +0530

    Add Footer component to RootLayout
    - Imported the Footer component into the RootLayout for improved layout structure.
    - Ensured the Header component remains in place for consistent navigation.
    
    This update enhances the overall user interface by including a footer section.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import Header from "@/components/shared/Header";
+import { Footer } from "@/components/shared/Footer";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/shared/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
